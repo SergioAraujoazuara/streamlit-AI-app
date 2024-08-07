e46e57171b69bbcda9db98cd4bbb9e8ad71a169e
msi <mario.segura.iglesias@gmail.com>
2024-08-07 11:59:44 +0200

    fix

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st
  
-
 with st.sidebar:
     open_api_key = st.text_input("Open AI API key", type='password')
 
+if not open_api_key:
+    st.warning("Please enter a valid Open AI API key.")
+    st.stop()
+st.session_state["api_key"] = open_api_key
 
 st.title("Talk with your IFC")
-st.write("Share your IFC and ask whats is happening")
+st.write("Share your IFC and ask what is happening")
 
 uploaded_file = st.file_uploader("Upload an article", type=(".ifc"))
 
@@ -18,3 +21,4 @@ question = st.text_input(
 
 "https://blog.streamlit.io/streamlit-authenticator-part-1-adding-an-authentication-component-to-your-app/"
 # next step
+
