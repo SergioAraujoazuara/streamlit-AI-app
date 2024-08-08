66fa7bcd931e0dca1e95fdf0748c0ae20a15e3a9
msi <mario.segura.iglesias@gmail.com>
2024-08-08 10:08:49 +0200

    functions added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
- 
+from functions import *
+
 with st.sidebar:
     open_api_key = st.text_input("Open AI API key", type='password')
 
@@ -11,14 +12,24 @@ st.session_state["api_key"] = open_api_key
 st.title("Talk with your IFC")
 st.write("Share your IFC and ask what is happening")
 
-uploaded_file = st.file_uploader("Upload an article", type=(".ifc"))
+uploaded_file = st.file_uploader("Upload file", type=(".ifc"))
+
+df = functions.transform_ifc_to_df(uploaded_file)
 
 question = st.text_input(
-    "Ask something about the article",
+    "Ask something about filee",
     placeholder="Can you give me a short summary?",
     disabled=not uploaded_file,
 )
 
+
+
+
+
+
+
+
+#! TODO
 "https://blog.streamlit.io/streamlit-authenticator-part-1-adding-an-authentication-component-to-your-app/"
 # next step
 
